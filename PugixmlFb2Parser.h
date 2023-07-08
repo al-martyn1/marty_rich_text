a61628947521802addc901cc22bf5ede3bee5f0e
al-martyn1 <amart@mail.ru>
2023-07-08 22:13:33 +0300

    Для примера отабличил описание методов PugixmlFb2Parser, заодно добавил для сравнения старую версию, в которой убрал даже попытки отабличивания возвращаемых значений от имен методов. Получилось реальное мясо.

--- PugixmlFb2Parser.h
+++ PugixmlFb2Parser.h
@@ -35,29 +35,56 @@ public:
 
     Para             parsePara(pugi::xml_node& node, std::unordered_set<std::string> *pProceseedAttrs);
 
-    Para             parseSubtitle(pugi::xml_node& node);
-    Para             parseImage(pugi::xml_node& node);
-    Para             parseTextAuthor(pugi::xml_node& node);
-    Para             parseStanzaV(pugi::xml_node& node);
+    Para             parseSubtitle       (pugi::xml_node& node);
+    Para             parseImage          (pugi::xml_node& node);
+    Para             parseTextAuthor     (pugi::xml_node& node);
+    Para             parseStanzaV        (pugi::xml_node& node);
 
-    ArtworkInfo      parseArtworkInfo(pugi::xml_node& fb2DescriptionNode);
-    AuthorInfo       parseAuthorInfo(pugi::xml_node& node);
-    TranslaterInfo   parseTranslaterInfo(pugi::xml_node& node);
-    DateInfo         parseDateInfo(pugi::xml_node& node);
-    DateRangeInfo    parseDateRangeInfo(pugi::xml_node& node);
-    DocumentInfo     parseDocumentInfo(pugi::xml_node& fb2DescriptionNode);
-    PublishingInfo   parsePublishingInfo(pugi::xml_node& fb2DescriptionNode);
-    DocumentHeader   parseDocumentHeader(pugi::xml_node& fb2DescriptionNode);
-    Title            parseTitle(pugi::xml_node& node);
-    Stanza           parseStanza(pugi::xml_node& node);
-    TdTh             parseTdTh(pugi::xml_node& node, const std::string &tagName);
-    Tr               parseTr(pugi::xml_node& node);
-    Table            parseTable(pugi::xml_node& node);
-    Poem             parsePoem(pugi::xml_node& node);
-    CiteEpigraph     parseCite(pugi::xml_node& node);
-    CiteEpigraph     parseEpigraph(pugi::xml_node& node);
-    Section          parseSection(pugi::xml_node& node);
+    ArtworkInfo      parseArtworkInfo    (pugi::xml_node& fb2DescriptionNode);
+    DocumentInfo     parseDocumentInfo   (pugi::xml_node& fb2DescriptionNode);
+    PublishingInfo   parsePublishingInfo (pugi::xml_node& fb2DescriptionNode);
+    DocumentHeader   parseDocumentHeader (pugi::xml_node& fb2DescriptionNode);
 
+    TdTh             parseTdTh           (pugi::xml_node& node, const std::string &tagName);
+
+    AuthorInfo       parseAuthorInfo     (pugi::xml_node& node);
+    TranslaterInfo   parseTranslaterInfo (pugi::xml_node& node);
+    DateInfo         parseDateInfo       (pugi::xml_node& node);
+    DateRangeInfo    parseDateRangeInfo  (pugi::xml_node& node);
+    Title            parseTitle          (pugi::xml_node& node);
+    Stanza           parseStanza         (pugi::xml_node& node);
+    Tr               parseTr             (pugi::xml_node& node);
+    Table            parseTable          (pugi::xml_node& node);
+    Poem             parsePoem           (pugi::xml_node& node);
+    CiteEpigraph     parseCite           (pugi::xml_node& node);
+    CiteEpigraph     parseEpigraph       (pugi::xml_node& node);
+    Section          parseSection        (pugi::xml_node& node);
+
+
+// Real meat goes here
+#if 0 
+    Para parseSubtitle(pugi::xml_node& node);
+    Para parseImage(pugi::xml_node& node);
+    Para parseTextAuthor(pugi::xml_node& node);
+    Para parseStanzaV(pugi::xml_node& node);
+    ArtworkInfo parseArtworkInfo(pugi::xml_node& fb2DescriptionNode);
+    DocumentInfo parseDocumentInfo(pugi::xml_node& fb2DescriptionNode);
+    PublishingInfo parsePublishingInfo(pugi::xml_node& fb2DescriptionNode);
+    DocumentHeader parseDocumentHeader(pugi::xml_node& fb2DescriptionNode);
+    TdTh parseTdTh(pugi::xml_node& node, const std::string &tagName);
+    AuthorInfo parseAuthorInfo(pugi::xml_node& node);
+    TranslaterInfo parseTranslaterInfo(pugi::xml_node& node);
+    DateInfo parseDateInfo(pugi::xml_node& node);
+    DateRangeInfo parseDateRangeInfo(pugi::xml_node& node);
+    Title parseTitle(pugi::xml_node& node);
+    Stanza parseStanza(pugi::xml_node& node);
+    Tr parseTr(pugi::xml_node& node);
+    Table parseTable(pugi::xml_node& node);
+    Poem parsePoem(pugi::xml_node& node);
+    CiteEpigraph parseCite(pugi::xml_node& node);
+    CiteEpigraph parseEpigraph(pugi::xml_node& node);
+    Section parseSection(pugi::xml_node& node);
+#endif
 
 }; // class PugixmlFb2Parser
 
